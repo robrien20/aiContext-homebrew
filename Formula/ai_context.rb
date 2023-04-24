cc369f94e5abb08a1b9d72a8707b5640a53670e3
Robert O'Brien <robrien20@gmail.com>
2023-04-24 19:53:16 -0400

    Update ai_context.rb

--- Formula/ai_context.rb
+++ Formula/ai_context.rb
@@ -1,9 +1,9 @@
 class AiContext < Formula
   desc "A script to provide context and project information to AI assistants like ChatGPT-4"
   homepage "https://github.com/robrien20/aiContext"
-  url "https://api.github.com/repos/robrien20/aiContext/tarball/0.1.2"
-  version "0.1.2"
-  sha256 "d64d25c5c34ba8d24fcf8553931c0f3c253efa4893daa9e8e29e70e65b17727f"
+  url "https://api.github.com/repos/robrien20/aiContext/tarball/0.1.3"
+  version "0.1.3"
+  sha256 "c742a1c6c5f9895bfac0f240d6a8f608fdacfbf12b582fde9f0023bf6903e9ef"
 
   def install
     bin.install "aiContext"
